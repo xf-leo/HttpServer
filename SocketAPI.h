0c2ff1cdffa97e72c6c255cfe8c67edfdfc39a37
xf-leo <15251765089@163.COM>
2016-02-28 13:17:01 +0800

    Update SocketAPI.h

--- SocketAPI.h
+++ SocketAPI.h
@@ -8,7 +8,7 @@ int createNonBlockingSocket(sa_family_t family);
 int bind(int fd,const struct sockaddr* addr);
 int connect(int fd,const struct sockaddr* addr);
 int listen(int fd);
-int accept(iny fd,struct sockaddr* addr);
+int accept(int fd,struct sockaddr* addr);
 ssize_t write(int fd,const void* buf,size_t count);
 ssize_t read(int fd,void* buf,size_t count);
 }
