052f30d00f5d424eb7e652a6f86e34a7d0690195
xf-leo <15251765089@163.COM>
2016-02-28 13:01:48 +0800

    Create SocketAPI.h

---
+++ SocketAPI.h
@@ -0,0 +1,15 @@
+
+
+namespace muaiwu
+{
+namespace sockets
+{
+int createNonBlockingSocket(sa_family_t family);
+int bind(int fd,const struct sockaddr* addr);
+int connect(int fd,const struct sockaddr* addr);
+int listen(int fd);
+int accept(iny fd,struct sockaddr* addr);
+ssize_t write(int fd,const void* buf,size_t count);
+ssize_t read(int fd,void* buf,size_t count);
+}
+}
